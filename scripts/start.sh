5d23a955a07a6b84961fed44c7e08e74077e2ee3
MadhumithaRJ <86790772+MadhumithaRJ@users.noreply.github.com>
2025-03-06 14:18:29 -0500

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,4 @@
-
 #!/bin/bash
-
-echo "Running start_app.sh"
-
-# Start the application (assuming PM2 is being used for process management)
-cd /var/www/html
-pm2 start app.js --name "my-app"  # Replace "app.js" with the entry file of your app
-pm2 save
+echo "Starting the new version of the application..."
+cd /home/ec2-user/my-app
+npm start
