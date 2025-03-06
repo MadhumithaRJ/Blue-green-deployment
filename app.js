5a6a6a9b7c4ecccaf51d6fe2d374705dcd0ec427
MadhumithaRJ <86790772+MadhumithaRJ@users.noreply.github.com>
2025-03-05 21:53:46 -0500

    Update and rename index.html to app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Route for the homepage
+app.get('/', (req, res) => {
+    res.send('<h1>Hello from the Blue-Green Deployment!</h1><p>Version 1.0</p>');
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(⁠ Server is running on port ${PORT} ⁠);
+});
