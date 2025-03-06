007b75eb6dd4fcea981fd3b197ad45d9dfa5c772
MadhumithaRJ <86790772+MadhumithaRJ@users.noreply.github.com>
2025-03-06 14:17:50 -0500

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-echo "Running after_install.sh"
-
-# Install Node.js dependencies
-cd /var/www/html
+echo "Installing dependencies..."
+cd /home/ec2-user/my-app
 npm install
-
-# Set environment-specific configurations (optional)
-if [ "$ENV" == "blue" ]; then
-  cp /var/www/html/config/config-blue.json /var/www/html/config/config.json
-else
-  cp /var/www/html/config/config-green.json /var/www/html/config/config.json
-fi
